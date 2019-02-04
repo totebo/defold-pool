29550641199dbfa71ce04072254f68470573ea4e
Niclas Aberg <niclas@totebo.com>
2019-02-04 13:40:35 +0000

    Check if the object exists.

--- pool/pool.lua
+++ pool/pool.lua
@@ -8,18 +8,21 @@ M.pool = {}
 function M:add( id, object )
 
 	local pool = M.pool
-	
+
 	if not(pool[id]) then
 		pool[id] = {}
 	end
 	insert( pool[id], object )
-	
+
 end
 
 function M:remove( id )
 
 	local pool = M.pool
-	return remove( pool[id] )
+	local object = pool[id]
+	if object then
+		return remove( pool[id] )
+	end
 
 end
 
